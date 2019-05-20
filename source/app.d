ca52376a6563c91617945f9aa68aeef179c903ed
RustyRaptor <ziad.arafat@etvamerica.com>
2019-05-20 16:24:12 -0600

    added randomCol function, documented functions, added TODO lines

--- source/app.d
+++ source/app.d
@@ -2,26 +2,44 @@ import std.stdio;
 import std.algorithm.comparison : among, equal;
 import std.range : iota;
 import std.random;
+import std.datetime;
 
+/// Prints the board
 void printBoard(int[5][5] board){
     foreach(row; board) {
         writeln(row);
         }
 }
 
+/// Prints the welcome message
 void printWelcome(){
     writeln("Welcome to Baddleship!!");
 }
 
-void randomRow(){
+/// returns a random row on the game board
+int randomRow(){
     
     auto rnd = Random(22);
-    auto i = uniform(0, 15, rnd);
-    assert(i >= 0 && i < 15);
-    writeln(i);
+    /// TODO: range based on length of board
+    const int i = uniform(0, 4, rnd);
+    assert(i >= 0 && i < 4);
+    return(i);
+}
+
+/// returns a random column on the game board
+int randomCol(){
+    auto rnd = Random(22);
+
+    /// TODO: range based on length of board
+    const int i = uniform(0, 4, rnd);
+    assert(i >= 0 && i < 4);
+    return(i);
 }
 
 void main(){
+
+    /// Initialize the board
+    /// TODO: User inputs size of board
     int[5][5] board;
     foreach(row; board) {
         foreach(item; row){
@@ -30,6 +48,6 @@ void main(){
     }
     printWelcome();
     printBoard(board);
-    randomRow();
-    
+    writeln(randomRow());
+    writeln(randomCol());
 }
